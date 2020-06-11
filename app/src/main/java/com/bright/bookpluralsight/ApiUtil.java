2620d5144359c0c26557dd0a391ca2210a8f738e
bright <brightahedor@gmail.com>
2020-06-11 21:01:13 +0000

    added api util class

--- app/src/main/java/com/bright/bookpluralsight/ApiUtil.java
+++ app/src/main/java/com/bright/bookpluralsight/ApiUtil.java
@@ -1,18 +1,24 @@
 package com.bright.bookpluralsight;
 
+import android.util.Log;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Scanner;
 
 import javax.net.ssl.HttpsURLConnection;
 
 public class ApiUtil {
+
     private ApiUtil() {
     }
 
+    //MARK: base url to call
     public static final String BASE_URL = "https://www.googleapi.com/books/v1/volumes";
 
+    //MARK: build connection
     public static URL buildUrl(String title) {
         String fullUrl = BASE_URL + "?q=" + title;
         URL url = null;
@@ -24,10 +30,26 @@ public class ApiUtil {
         return url;
     }
 
+    //MARK: network connection
     public static String getJson(URL url) throws IOException {
         HttpsURLConnection httpsURLConnection = (HttpsURLConnection) url.openConnection();
-        InputStream inputStream = httpsURLConnection.getInputStream()
-
+        InputStream inputStream = httpsURLConnection.getInputStream();
+        Scanner scanner = new Scanner(inputStream);
+        scanner.useDelimiter("\\A");
+        try {
+            boolean hasNext = scanner.hasNext();
+            if (hasNext) {
+                return scanner.next();
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Log.e("", e.toString());
+            return null;
+        } finally {
+            httpsURLConnection.disconnect();
+        }
     }
 
 }
